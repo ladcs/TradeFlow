5efe715df047ead2d42ef32c4ee5219687376ab7
ladcs <luciano.silva@aidacx.com.br>
2026-03-31 21:38:13 -0300

    fix: using TradeFlow.Orders.Models

--- TradeFlow.Orders/TableConfigurations/OrderConfiguration.cs
+++ TradeFlow.Orders/TableConfigurations/OrderConfiguration.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using TradeFlow.Orders.models.Orders;
+using TradeFlow.Orders.Models;
 
 namespace TradeFlow.Orders.TableConfigurations;
 
